caa5a4c420db75a97c33c10d5520a52c49a63c68
Rob Armes <robarmes@hotmail.com>
2018-09-24 10:25:37 +0100

    routes fixed

--- app/routes.js
+++ app/routes.js
@@ -3,22 +3,29 @@ const router = express.Router()
 
 // Route index page
 router.get('/', function (req, res) {
-  res.render('index')
+  res.render('index');
 })
 
-router.post('/service-payment-screen', function (req, res) {
+
+router.post('/payment-screens/service-payment-screen', function (req, res) {
 
  // Make a variable and give it the value from 'juggling-balls'
  var paymentType = req.session.data['payment-type']
 
+ console.log( paymentType );
+
  // Check whether the variable matches a condition
  if (paymentType == "cheque"){
    // Send user to next page
-   res.redirect('/service-screens/complete')
+   res.redirect('/payment-screens/complete')
+ }
+ if (paymentType == "your-account"){
+   // Send user to next page
+   res.redirect('/payment-screens/select-account')
  }
  else {
    // Send user to ineligible page
-   res.redirect('/payment-screens/HWF')
+   res.redirect('/payment-screens/HWF-ref')
  }
 
 })
